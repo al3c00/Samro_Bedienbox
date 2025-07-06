767c0d331dcef76552b3b22500a2fd7fbf2a2572
Alec Fankhauser <alec.fank@gmail.com>
2025-07-06 21:51:29 +0200

    improved loop, output-values are only refreshed when new input values are registered

--- Samro_Bedienbox.ino
+++ Samro_Bedienbox.ino
@@ -16,7 +16,6 @@
 #define _Joystick_Left_Y	A0
 #define _Joystick_Right_X	A3
 #define _Joystick_Right_Y	A2
-#define _Button_Input		3
 
 
 
@@ -36,19 +35,24 @@
 
 struct Inputs
 {
-	uint16_t	Buttons; //2.5V = SW1, 3V = SW2, 1.88V = SW1 & SW2
 
-	uint16_t	Joystick_Left_X;
-	uint16_t	Joystick_Left_Y;
+	uint16_t	Joystick_Left_X_Value;
+	uint16_t	Joystick_Left_X_Measured;
+	uint16_t	Joystick_Left_Y_Value;
+	uint16_t	Joystick_Left_Y_Measured;
 
-	uint16_t	Joystick_Right_X;
-	uint16_t	Joystick_Right_Y;
+	uint16_t	Joystick_Right_X_Value;
+	uint16_t	Joystick_Right_X_Measured;
+	uint16_t	Joystick_Right_Y_Value;
+	uint16_t	Joystick_Right_Y_Measured;
 };
 Inputs input;
 
 
 uint16_t last_time;
 uint16_t interval_time;
+
+bool loop_New_Values = false;
 
 //Analog Inputs 0 - 1024
 //Analog Active 0 - 204 and 820 - 1024
@@ -65,7 +69,7 @@ void setup()
 		pinMode(_Joystick_Left_Y, INPUT);
 		pinMode(_Joystick_Right_X, INPUT);
 		pinMode(_Joystick_Right_Y, INPUT);
-		pinMode(_Button_Input, INPUT);
+
 
 		pinMode(_Func1, OUTPUT);
 		pinMode(_Func2, OUTPUT);
@@ -95,90 +99,115 @@ void loop()
 		last_time = current_time;
 
 		//Read left joystick
-		input.Joystick_Left_X = analogRead(_Joystick_Left_X);
-		input.Joystick_Left_Y = analogRead(_Joystick_Left_Y);
+		if ((input.Joystick_Left_X_Measured = analogRead(_Joystick_Left_X)) != input.Joystick_Left_X_Value)
+		{
+			loop_New_Values = true;
+		}
+		if ((input.Joystick_Left_Y_Measured = analogRead(_Joystick_Left_Y)) != input.Joystick_Left_Y_Value)
+		{
+			loop_New_Values = true;
+		}
 		//Read right joystick
-		input.Joystick_Right_X = analogRead(_Joystick_Right_X);
-		input.Joystick_Right_Y = analogRead(_Joystick_Right_Y);
-		//Read button 
-		input.Buttons = analogRead(_Button_Input);
+		if ((input.Joystick_Right_X_Measured = analogRead(_Joystick_Right_X)) != input.Joystick_Right_X_Value)
+		{
+			loop_New_Values = true;
+		}
+		if ((input.Joystick_Right_Y_Measured = analogRead(_Joystick_Right_Y)) != input.Joystick_Right_Y_Value)
+		{
+			loop_New_Values = true;
+		}
 
+
+
+
+		//If there are any new Input Values, go refresh everything
+		if (loop_New_Values)
+		{
 
 		
 
-		//Achse
-		{
-			if (input.Joystick_Left_X < Joystick_LowActive_Value)
+			//Achse
 			{
-				digitalWrite(_Deichsel_Links, HIGH);
-			}
-			else if (input.Joystick_Left_X >= Joystick_LowActive_Value && input.Joystick_Left_X < Joystick_HighActive_Value)
-			{
-				digitalWrite(_Deichsel_Links, LOW);
-				digitalWrite(_Deichsel_Rechts, LOW);
-			}
-			else if (input.Joystick_Left_X > Joystick_HighActive_Value)
-			{
-				digitalWrite(_Deichsel_Rechts, HIGH);
+				if (input.Joystick_Left_X_Measured < Joystick_LowActive_Value)
+				{
+					digitalWrite(_Deichsel_Links, HIGH);
+				}
+				else if (input.Joystick_Left_X_Measured >= Joystick_LowActive_Value && input.Joystick_Left_X_Measured < Joystick_HighActive_Value)
+				{
+					digitalWrite(_Deichsel_Links, LOW);
+					digitalWrite(_Deichsel_Rechts, LOW);
+				}
+				else if (input.Joystick_Left_X_Measured > Joystick_HighActive_Value)
+				{
+					digitalWrite(_Deichsel_Rechts, HIGH);
+				}
+
+
 			}
 
-			
-		}
 
-		
-		//Deichsel
-		{
-			if (input.Joystick_Left_Y < Joystick_LowActive_Value)
+			//Deichsel
 			{
-				digitalWrite(_Achse_Links, HIGH);
+				if (input.Joystick_Left_Y_Measured < Joystick_LowActive_Value)
+				{
+					digitalWrite(_Achse_Links, HIGH);
+				}
+				else if (input.Joystick_Left_Y_Measured >= Joystick_LowActive_Value && input.Joystick_Left_Y_Measured < Joystick_HighActive_Value)
+				{
+					digitalWrite(_Achse_Links, LOW);
+					digitalWrite(_Achse_Rechts, LOW);
+				}
+				else if (input.Joystick_Left_Y_Measured > Joystick_HighActive_Value)
+				{
+					digitalWrite(_Achse_Rechts, HIGH);
+				}
 			}
-			else if (input.Joystick_Left_Y >= Joystick_LowActive_Value && input.Joystick_Left_Y < Joystick_HighActive_Value)
-			{
-				digitalWrite(_Achse_Links, LOW);
-				digitalWrite(_Achse_Rechts, LOW);
-			}
-			else if (input.Joystick_Left_Y > Joystick_HighActive_Value)
-			{
-				digitalWrite(_Achse_Rechts, HIGH);
-			}
-		}
 
-		//Func--Dammaufnahme oder Bunker
-		{
-			if (input.Joystick_Right_Y < Joystick_LowActive_Value)
+			//Func--Dammaufnahme oder Bunker
 			{
-				digitalWrite(_Func2, HIGH);
+				if (input.Joystick_Right_Y_Measured < Joystick_LowActive_Value)
+				{
+					digitalWrite(_Func2, HIGH);
+				}
+				else if (input.Joystick_Right_Y_Measured >= Joystick_LowActive_Value && input.Joystick_Right_Y_Measured < Joystick_HighActive_Value)
+				{
+					digitalWrite(_Func1, LOW);
+					digitalWrite(_Func2, LOW);
+				}
+				else if (input.Joystick_Right_Y_Measured > Joystick_HighActive_Value)
+				{
+					digitalWrite(_Func1, HIGH);
+				}
 			}
-			else if (input.Joystick_Right_Y >= Joystick_LowActive_Value && input.Joystick_Right_Y < Joystick_HighActive_Value)
-			{
-				digitalWrite(_Func1, LOW);
-				digitalWrite(_Func2, LOW);
-			}
-			else if (input.Joystick_Right_Y > Joystick_HighActive_Value)
-			{
-				digitalWrite(_Func1, HIGH);
-			}
-		}
 
-		//Schnabel
-		{
-			if (input.Joystick_Right_X < Joystick_LowActive_Value)
+			//Schnabel
 			{
-				digitalWrite(_Schnabel_Runter, HIGH);
+				if (input.Joystick_Right_X_Measured < Joystick_LowActive_Value)
+				{
+					digitalWrite(_Schnabel_Runter, HIGH);
+				}
+				else if (input.Joystick_Right_X_Measured >= Joystick_LowActive_Value && input.Joystick_Right_X_Measured < Joystick_HighActive_Value)
+				{
+					digitalWrite(_Schnabel_Runter, LOW);
+					digitalWrite(_Schnabel_Hoch, LOW);
+				}
+				else if (input.Joystick_Right_X_Measured > Joystick_HighActive_Value)
+				{
+					digitalWrite(_Schnabel_Hoch, HIGH);
+				}
 			}
-			else if (input.Joystick_Right_X >= Joystick_LowActive_Value && input.Joystick_Right_X < Joystick_HighActive_Value)
-			{
-				digitalWrite(_Schnabel_Runter, LOW);
-				digitalWrite(_Schnabel_Hoch, LOW);
-			}
-			else if (input.Joystick_Right_X > Joystick_HighActive_Value)
-			{
-				digitalWrite(_Schnabel_Hoch, HIGH);
-			}
+
+			input.Joystick_Left_X_Value = input.Joystick_Left_X_Measured;
+			input.Joystick_Left_Y_Value = input.Joystick_Left_Y_Measured;
+			input.Joystick_Right_X_Value = input.Joystick_Right_X_Measured;
+			input.Joystick_Right_Y_Value = input.Joystick_Right_Y_Measured;
+
+			loop_New_Values = false;
+
+
 		}
 
 	}
-	
 
 
 /*
